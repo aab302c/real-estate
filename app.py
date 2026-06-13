c3cb886ce0192f2b8ae21c8327e8ae5a7e8b8624
alexb <oksstudio@mail.ru>
2026-06-13 22:55:30 +0300

    Update app.py

--- app.py
+++ app.py
@@ -101,9 +101,7 @@ for building in buildings_data:
     if building["id"] == 43:
         building["lat"] = 60.066549
         building["lon"] = 30.303741
-    if building["id"] == 24:
-        building["lat"] = 60.027398
-        building["lon"] = 30.369055    
+   
     street, house = parse_address(building["address"])
     building["street"] = street
     building["house"] = house
